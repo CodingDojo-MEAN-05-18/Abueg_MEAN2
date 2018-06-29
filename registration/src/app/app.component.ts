391c0d1d1cdc62e64d02829f855da8118cbc79dd
Nicci Lee Abueg <nicci.abueg@gmail.com>
2018-06-29 11:36:08 -0700

    update reg

--- registration/src/app/app.component.ts
+++ registration/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,21 @@ import { User } from './user';
 export class AppComponent {
   user = new User();
   users = [];
-  onSubmit() {
+  formName: string = '';
+  formEmail: string = '';
+  formStatement: string = '';
+
+  onSubmit(event: Event, formData: NgForm) {
+    console.log(formData);
+    console.log(this.user);
+
+    if (formData.valid) {
+      this.formName = this.user.first_name;
+      this.formEmail = this.user.email;
+    }
+
     this.users.push(this.user);
     this.user = new User();
+    formData.reset();
   }
 }
